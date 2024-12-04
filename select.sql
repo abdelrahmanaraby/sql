df039559948acc0fe9069b0d1cd52d643e2a7e6e
abdel-rahman-araby890 <arabyabdelrahman81@gmail.com>
2024-12-05 00:27:15 +0200

    select statement

---
+++ select.sql
@@ -0,0 +1,9 @@
+USE AMITDB;
+-- SELECT  STATEMENT 
+-- And print all table 
+SELECT *FROM EMPLOYE;
+-- call - part  data 
+-- SELECT ID ,employe_NAME FROM EMPLOYE;
+
+--- select distinct
+SELECT DISTINCT city FROM EMPLOYE ;
